2778c1cd1f57de23a72c6d6a85020a717e065b50
mahfoo75 <mahfooz.lakhani@gmail.com>
2022-07-15 09:44:46 +0530

    Added JsonSerialize support and vreated JsonProducer

---
+++ src/main/java/com/selftech/kafkatutorial/kafka/JsonKafkaProducer.java
@@ -0,0 +1,31 @@
+package com.selftech.kafkatutorial.kafka;
+
+import com.selftech.kafkatutorial.constant.KafkaConstant;
+import com.selftech.kafkatutorial.payload.User;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class JsonKafkaProducer {
+
+    private KafkaTemplate<String, User> kafkaTemplate;
+
+    public JsonKafkaProducer(KafkaTemplate<String, User> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendMessage(User data) {
+        log.info(String.format("Message sent -> %s", data.toString()));
+        Message<User> message = MessageBuilder
+                .withPayload(data)
+                .setHeader(KafkaHeaders.TOPIC, KafkaConstant.JAVA_GUIDE_TOPIC)
+                .build();
+        kafkaTemplate.send(message);
+    }
+
+}
